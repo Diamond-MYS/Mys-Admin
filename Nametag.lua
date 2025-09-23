debfb5caf969e3cd0c09aa9c5a65c520b4ae4bab
Diamond-MYS <230668128+Diamond-MYS@users.noreply.github.com>
2025-09-23 01:05:02 -0400

    Update Nametag.lua

--- Nametag.lua
+++ Nametag.lua
@@ -11,7 +11,7 @@ end
 
 local Owners = {"01xMYS", "Strixxsy", "01xSILENCE"}
 local CoOwner = {"e2edeq", "jtyrjetahtersjrt"}
-local MysStaff = {"Rebootedpoppyrea8", "Dogenurgos", "deadvibexzs", "Jemenzboy1090", "chip_xoxalt"}
+local MysStaff = {"Rebootedpoppyrea8", "Dogenurgos", "deadvibexzs", "chip_xoxalt"}
 local Advertisers = {}
 local Helper = {}
 local Giveaway1 = {"Dupersigmadupe"}
