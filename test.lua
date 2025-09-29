a45ff9e4b80bf0588e6adb92abdda52a220e268a
mys <bodhistarr.co@gmail.com>
2025-09-29 15:55:54 -0400

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,17 @@
+-- whitelist.lua
+whitelist = {
+    ["01xMYS"] = "owner",
+    ["notmys67"] = "owner",
+    ["01xSILENCE"] = "owner",
+    ["Strixxsy"] = "owner",
+    ["e2edeq"] = "coowner",
+    ["jtyrjetahtersjrt"] = "coowner",
+    ["chip_xoxalt"] = "admin",
+    ["Dogenurgos"] = "moderator",
+    ["deadvibexzs"] = "headadmin",
+    ["Rebootedpoppyrea8"] = "admin",
+    ["pinksman18111"] = "moderator",
+    ["ueidhxbsjjsjnc"] = "moderator",
+    ["minette7ws"] = "moderator",
+    ["gotonlyfent"] = "moderator"
+}
