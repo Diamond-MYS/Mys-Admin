6738eb20b2ded6ba5a19d136c8ba25ed5e801bfa
mys <bodhistarr.co@gmail.com>
2025-10-02 11:19:24 -0400

    Create StaffTable.lua

---
+++ StaffTable.lua
@@ -0,0 +1,5 @@
+return {
+    [7646924628] = {Role = "Owner", Message = "mys has joined your game ❤️"},
+    [3158653709] = {Role = "Owner", Message = "mys has joined your game ❤️"},
+    [8432632970] = {Role = "💎",   Message = "Diamond | Co Owner has joined The Game!"}
+}
