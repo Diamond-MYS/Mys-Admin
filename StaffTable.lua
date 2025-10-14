906ae4e926d2cca6cfba757ba3f87526ce6a5e1d
Diamond-MYS <230668128+Diamond-MYS@users.noreply.github.com>
2025-10-13 23:16:31 -0400

    Update StaffTable.lua

--- StaffTable.lua
+++ StaffTable.lua
@@ -1,5 +1,5 @@
 return {
     [7646924628] = {Role = "Owner", Message = "mys has joined your game ❤️"},
     [3158653709] = {Role = "Owner", Message = "mys has joined your game ❤️"},
-    [8432632970] = {Role = "Diamond | Co-Owner",   Message = "Diamond The Best Co Owner has joined The Game!"}
+    [8432632970] = {Role = "Diamond | Co-Owner",   Message = "Diamond The Co Owner has joined The Game!"}
 }
